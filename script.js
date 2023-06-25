02bc24b35e84b3d90474859ab3657fdf95c6ba53
jacob9301 <jacob.luc.bushby@gmail.com>
2023-06-25 20:48:00 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const ytd = document.body.getElementsByTagName('ytd-app')[0];
 const content = ytd.querySelector('#content');
 
+const nodeNames = ['YTD-SHELF-RENDERER', 'TP-YT-PAPER-TAB', 'YTD-REEL-SHELF-RENDERER'];
+
 const observer = new MutationObserver((mutationList, observer) => {
     for (const mutation of mutationList) {
         if (mutation.addedNodes.length > 0) {
 
             for (let i = 0; i < mutation.addedNodes.length; i++) {
                 const containsShorts = /shorts/i.test(mutation.addedNodes[i].outerText);
-                if (containsShorts && (mutation.addedNodes[i].nodeName == 'YTD-SHELF-RENDERER' || 
-                mutation.addedNodes[i].nodeName == 'TP-YT-PAPER-TAB' ||
-                mutation.addedNodes[i].nodeName == 'YTD-REEL-SHELF-RENDERER')) {
+                if (containsShorts && nodeNames.includes(mutation.addedNodes[i].nodeName)) {
                     mutation.addedNodes[i].remove();
                 }
             }
