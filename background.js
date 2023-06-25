d83f2061e7e1d2979fe2d52e27114188dcf7be3e
jacob9301 <jacob.luc.bushby@gmail.com>
2023-06-25 03:21:15 +0100

    modified:   background.js

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
+const redirect = (details) => {
     if (details.url.includes("www.youtube.com/shorts")) {
         chrome.tabs.update(details.tabId, { url: "https://youtube.com" });
     }
-  });
+} 
+
+chrome.webNavigation.onBeforeNavigate.addListener(redirect);
